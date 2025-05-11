d6fb9c00f3d5b87e2be5a4c9f65deb539de7c49e
Markus Schlichting <markus.schlichting@karakun.com>
2025-05-11 21:57:28 +0200

    optimize table format

--- uml-doc-extract.py
+++ uml-doc-extract.py
@@ -6,7 +6,7 @@ def parse_uml_to_asciidoc(uml_file_path, output_file_path):
         uml_content = file.read()
 
     classes = re.findall(r"class\s+(\w+)\s*\{(?:\s*/' adoc ([^\n]*) '/)?([\s\S]*?)\}", uml_content)
-    relations = re.findall(r'(\w+)(?:\s*"([^"]*)"\s*)?\s*(<\|--|\*--|o--|--)(?:\s*"([^"]*)"\s*)?\s*(\w+)(?:\s*:\s*/\'\s*adoc-relation\s*(.*?)\s*\'/)?', uml_content)
+    relations = re.findall(r'(\w+)(?:\s*"([^"]*)"\s*)?\s*(<\|--|\*--|o--|--)(?:\s*"([^"]*)"\s*)?\s*(\w+)(?:\s*:\s*/\' adoc-relation (.*?) \'/)?', uml_content)
 
     with open(output_file_path, 'w') as file:
         for class_name, class_comment, class_body in classes:
@@ -18,6 +18,7 @@ def parse_uml_to_asciidoc(uml_file_path, output_file_path):
 
             if properties:
                 file.write("=== Eigenschaften\n\n")
+                file.write("[stripes=odd,options=header]\n")
                 file.write("|===\n| *Sichtbarkeit* | *Name* | *Typ* | *Beschreibung*\n")
                 for visibility, name, type_, description in properties:
                     visibility_word = "öffentlich" if visibility == "+" else "privat"
@@ -26,6 +27,7 @@ def parse_uml_to_asciidoc(uml_file_path, output_file_path):
 
             if methods:
                 file.write("=== Methoden\n\n")
+                file.write("[stripes=odd,options=header]\n")
                 file.write("|===\n| *Sichtbarkeit* | *Name* | *Rückgabetyp* | *Beschreibung*\n")
                 for visibility, name, return_type, description in methods:
                     visibility_word = "öffentlich" if visibility == "+" else "privat"
@@ -35,6 +37,7 @@ def parse_uml_to_asciidoc(uml_file_path, output_file_path):
             class_relations = [rel for rel in relations if class_name == rel[0] or class_name == rel[4]]
             if class_relations:
                 file.write("=== Beziehungen\n\n")
+                file.write("[stripes=odd,options=header]\n")
                 file.write("|===\n| *Von* | *Kardinalität* | *Typ* | *Kardinalität* | *Zu* | *Beschreibung*\n")
                 for from_class, from_card, rel_type, to_card, to_class, description in class_relations:
                     rel_type_word = {
